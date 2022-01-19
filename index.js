840c9a5f41e81fd96a4fd0067235a411bff760c4
Baptiste <baptiste.lecat44@gmail.com>
2022-01-19 18:36:54 +0100

    Update index.js
    Implement API call.
    Handle disconnect.

--- index.js
+++ index.js
@@ -4,33 +4,60 @@ const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+const { callNewsAPI } = require("./services/apiNewsCaller.js");
+const { UserData } = require("./src/model/UserData.js");
 
 io.on("connection", (socket) => {
     console.log("a user connected");
 
     var userData = null;
+    var interval;
+
+    socket.on("disconnect", () => {
+        console.log(interval);
+        clearInterval(interval);
+        console.log(interval);
+    });
 
     //Le client demande expressement des données
     socket.on("getNews", () => {
         if (userData != null) {
-            console.log(userData);
-            console.log("Voici des news");
+            callNewsAPI(userData).then((data) => {
+                console.log("Send API data.");
+                socket.emit("getNewsAuto", {
+                    data: data,
+                });
+            });
         } else {
             console.log("Veuillez d'abord fournir vos préférences utilisateur.");
         }
     });
 
+
+
     //Le client nous envoie ses filtres pour l'api d'actualité.
     socket.on("setUserData", (sendedUserData) => {
-        userData = sendedUserData;
-        console.log("Préférences utilisateur définies.");
-        //On envoi de nous même au client les données
-        setInterval(function() {
-            //appel api vers l'api d'actualité
-            socket.emit("getNewsAuto", {
-                uneNews: "Beau temps today",
-            });
-        }, 5000);
+        userData = new UserData(
+            sendedUserData.lang,
+            sendedUserData.countries,
+            sendedUserData.topic,
+            sendedUserData.sources
+        );
+        if (userData != null) {
+            console.log("Préférences utilisateur définies.");
+            //On envoi de nous même au client les données
+            interval = setInterval(function() {
+                //appel api vers l'api d'actualité
+                callNewsAPI(userData).then((data) => {
+                    console.log("Send API data.")
+                    socket.emit("getNewsAuto", {
+                        data: data,
+                    });
+                });
+            }, 10000);
+        } else {
+            console.log("Merci de spécifier des préférences utilisateur.")
+        }
     });
 });
 

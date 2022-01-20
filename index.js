a3d783d664369ce40a5fd0fee8c921ae083be882
Baptiste <baptiste.lecat44@gmail.com>
2022-01-20 17:04:17 +0100

    Implement refreshCycleService

--- index.js
+++ index.js
@@ -3,69 +3,77 @@ const app = express();
 const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
+const { setTimeout } = require("timers/promises");
 const io = new Server(server);
+
 const { callNewsAPI } = require("./services/apiNewsCaller.js");
+const {
+  getTimeBeforeStartCycle,
+  getTimeCycle,
+} = require("./services/refreshCycleService");
 const { UserData } = require("./src/model/UserData.js");
 
 io.on("connection", (socket) => {
-    console.log("a user connected");
+  console.log("a user connected");
 
-    var userData = null;
-    var interval;
+  var userData = null;
+  var interval;
 
-    socket.on("disconnect", () => {
-        console.log(interval);
-        clearInterval(interval);
-        console.log(interval);
-    });
+  socket.on("disconnect", () => {
+    console.log(interval);
+    clearInterval(interval);
+    console.log(interval);
+  });
 
-    //Le client demande expressement des données
-    socket.on("getNews", () => {
-        if (userData != null) {
-            callNewsAPI(userData).then((data) => {
-                console.log("Send API data.");
-                socket.emit("getNewsAuto", {
-                    data: data,
-                });
+  //Le client demande expressement des données
+  socket.on("getNews", () => {
+    if (userData != null) {
+      callNewsAPI(userData).then((data) => {
+        console.log("Send API data.");
+        socket.emit("getNewsAuto", {
+          data: data,
+        });
+      });
+    } else {
+      console.log("Veuillez d'abord fournir vos préférences utilisateur.");
+    }
+  });
+
+  //Le client nous envoie ses filtres pour l'api d'actualité.
+  socket.on("setUserData", (sendedUserData) => {
+    userData = new UserData(
+      sendedUserData.lang,
+      sendedUserData.countries,
+      sendedUserData.topic,
+      sendedUserData.sources
+    );
+    if (userData != null) {
+      console.log("Préférences utilisateur définies.");
+      //On envoi de nous même au client les données
+      callNewsAPI(userData).then((data) => {
+        console.log("Send API data.");
+        socket.emit("getNewsAuto", {
+          data: data,
+        });
+      });
+      setTimeout(() => {
+        //Puis tout les X time on actualise ces données.
+        interval = setInterval(function () {
+          //appel api vers l'api d'actualité
+          callNewsAPI(userData).then((data) => {
+            console.log("Send API data.");
+            socket.emit("getNewsAuto", {
+              data: data,
             });
-        } else {
-            console.log("Veuillez d'abord fournir vos préférences utilisateur.");
-        }
-    });
-
-    //Le client nous envoie ses filtres pour l'api d'actualité.
-    socket.on("setUserData", (sendedUserData) => {
-        userData = new UserData(
-            sendedUserData.lang,
-            sendedUserData.countries,
-            sendedUserData.topic,
-            sendedUserData.sources
-        );
-        if (userData != null) {
-            console.log("Préférences utilisateur définies.");
-            //On envoi de nous même au client les données
-            callNewsAPI(userData).then((data) => {
-                console.log("Send API data.");
-                socket.emit("getNewsAuto", {
-                    data: data,
-                });
-            });
-            //Puis tout les X time on actualise ces données.
-            interval = setInterval(function() {
-                //appel api vers l'api d'actualité
-                callNewsAPI(userData).then((data) => {
-                    console.log("Send API data.")
-                    socket.emit("getNewsAuto", {
-                        data: data,
-                    });
-                });
-            }, 10000);
-        } else {
-            console.log("Merci de spécifier des préférences utilisateur.")
-        }
-    });
+          });
+        }, getTimeCycle());
+      }, getTimeBeforeStartCycle());
+    } else {
+      console.log("Merci de spécifier des préférences utilisateur.");
+    }
+  });
 });
 
 server.listen(3000, () => {
-    console.log("listening on *:3000");
+  console.log("listening on *:3000");
 });
---
+++ services/refreshCycleService.js
@@ -0,0 +1,109 @@
+const { SETTINGS } = require("../config/settings");
+const datejs = require("datejs");
+
+function getTimeBeforeStartCycle() {
+  /**
+   * We define once time the date time now var.
+   * Because if our program take a little bit time to execute we can have a difference of time,
+   * (day if the program start at 11:59pm) between now instance.
+   */
+  var now = new Date();
+  console.log("Heure actuelle: " + now.toTimeString());
+
+  var firstTime = new Date(now).set({
+    hour: SETTINGS.TIME_REFRESH.FIRST_REFRESH,
+    minute: 0,
+    second: 0,
+    millisecond: 0,
+  });
+  var secondTime = new Date(now).set({
+    hour: SETTINGS.TIME_REFRESH.LAST_REFRESH,
+    minute: 0,
+    second: 0,
+    millisecond: 0,
+  });
+  var timeBeforeCycle;
+
+  // If now is not between firstTime and SecondTime
+  if (now < firstTime || now > secondTime) {
+    //If secondTime is before now, we need to to use the firstTime for the next day.
+    if (now > secondTime) {
+      firstTime.addDays(1);
+      //Calculate the time range to see if we need to bypass the or not the firstTime of refresh.
+      timeBeforeCycle = getTimeBeforeCycleWithTimeRangeFirstTime(
+        firstTime,
+        secondTime,
+        now
+      );
+    } else {
+      timeBeforeCycle = getTimeBeforeCycleWithTimeRangeFirstTime(
+        firstTime,
+        secondTime,
+        now
+      );
+    }
+  } else {
+    //Second Time
+    timeBeforeCycle = getTimeBeforeCycleWithTimeRangeSecondTime(
+      secondTime,
+      firstTime,
+      now
+    );
+  }
+
+  console.log(
+    "Horaire de début de cycle: " +
+      now.addMinutes(timeBeforeCycle).toTimeString()
+  );
+  console.log("Temps avant début du cycle (min): " + timeBeforeCycle);
+  return minutesToMilliseconds(timeBeforeCycle);
+}
+
+function getTimeBeforeCycleWithTimeRangeSecondTime(secondTime, firstTime, now) {
+  var timeRangeForRefresh = SETTINGS.TIME_REFRESH.TIME_BEFORE_REFRESH;
+  var timeRange = new Date(secondTime).addHours(-timeRangeForRefresh);
+  console.log("Time range: " + timeRange.toTimeString());
+  if (timeRange > now) {
+    timeBeforeCycle = diff_minutes(secondTime, now);
+  } else {
+    console.log("--- Dans la TimeRange ---");
+    firstTime.addDays(1);
+    timeBeforeCycle = diff_minutes(firstTime, now);
+  }
+  return timeBeforeCycle;
+}
+
+function getTimeBeforeCycleWithTimeRangeFirstTime(firstTime, secondTime, now) {
+  var timeRangeForRefresh = SETTINGS.TIME_REFRESH.TIME_BEFORE_REFRESH;
+  var timeRange = new Date(firstTime).addHours(-timeRangeForRefresh);
+  console.log("Time range: " + timeRange.toTimeString());
+  if (timeRange > now) {
+    timeBeforeCycle = diff_minutes(firstTime, now);
+  } else {
+    console.log("--- Dans la TimeRange ---");
+    timeBeforeCycle = diff_minutes(secondTime, now);
+  }
+
+  return timeBeforeCycle;
+}
+
+function diff_minutes(dt2, dt1) {
+  var diff = (dt2.getTime() - dt1.getTime()) / 1000;
+  diff /= 60;
+  return Math.abs(diff);
+}
+
+function minutesToMilliseconds(minutesTime) {
+  return minutesTime * 60 * 1000;
+}
+
+function hoursToMilliseconds(minutesTime) {
+  return minutesTime * 60 * 60 * 1000;
+}
+
+function getTimeCycle() {
+  return hoursToMilliseconds(
+    SETTINGS.TIME_REFRESH.LAST_REFRESH - SETTINGS.TIME_REFRESH.FIRST_REFRESH
+  );
+}
+module.exports = { getTimeBeforeStartCycle, getTimeCycle };

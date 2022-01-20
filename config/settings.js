dd2cadc1656342df5ad05aaf84d8f17da96abca5
Baptiste <baptiste.lecat44@gmail.com>
2022-01-20 17:04:47 +0100

    Create settings.js

---
+++ config/settings.js
@@ -0,0 +1,14 @@
+const SETTINGS = {
+
+    AXIOS_API: {
+        BASE_URL: "https://api.newscatcherapi.com/v2/",
+    },
+    TIME_REFRESH: {
+        FIRST_REFRESH: 7,
+        LAST_REFRESH: 19,
+        TIME_BEFORE_REFRESH: 4
+    }
+
+}
+
+exports.SETTINGS = SETTINGS

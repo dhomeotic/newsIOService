d165ff528ca5e706a038a25f449a3c2f0cd718b0
Samuel De Oliveira <samuel.deoliveir1@gmail.com>
2022-01-24 22:10:34 +0100

    Update settings.js

--- config/settings.js
+++ config/settings.js
@@ -2,6 +2,7 @@ const SETTINGS = {
 
     AXIOS_API: {
         BASE_URL: "https://api.newscatcherapi.com/v2/",
+        API_KEY: "KJmaITz--gyy0Nh6M18LYS_Jvxb50mVpmAasqdtyxBw"
     },
     TIME_REFRESH: {
         FIRST_REFRESH: 0,

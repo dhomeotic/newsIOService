db31774af5522c82cbe91b23bdbd98163ddb1533
BaptisteLecat <60200125+BaptisteLecat@users.noreply.github.com>
2022-01-24 21:24:19 +0100

    Make cycleRefreshWorking

--- index.js
+++ index.js
@@ -8,77 +8,61 @@ const io = new Server(server);
 
 const { callNewsAPI } = require("./services/apiNewsCaller.js");
 const {
-  getTimeBeforeStartCycle,
-  getTimeCycle,
+    startCycle,
 } = require("./services/refreshCycleService");
 const { UserData } = require("./src/model/UserData.js");
 
-io.on("connection", (socket) => {
-  console.log("a user connected");
+io.on("connection", async function(socket) {
+    console.log("a user connected");
 
-  var userData = null;
-  var interval;
+    var userData = null;
+    var interval;
 
-  socket.on("disconnect", () => {
-    clearInterval(interval);
-  });
+    socket.on("disconnect", () => {
+        clearInterval(interval);
+    });
 
-  //Le client demande expressement des données
-  socket.on("getNews", () => {
-    if (userData != null) {
-      callNewsAPI(userData).then((data) => {
-        console.log("Send API data.");
-        socket.emit("getNewsAuto", {
-          data: data,
-        });
-      });
-    } else {
-      console.log("Veuillez d'abord fournir vos préférences utilisateur.");
-    }
-  });
-
-  //Le client nous envoie ses filtres pour l'api d'actualité.
-  socket.on("setUserData", (sendedUserData) => {
-    userData = new UserData(
-      sendedUserData.lang,
-      sendedUserData.countries,
-      sendedUserData.topic,
-      sendedUserData.sources
-    );
-    if (userData != null) {
-      console.log("Préférences utilisateur définies.");
-      //On envoi de nous même au client les données
-      callNewsAPI(userData).then((data) => {
-        console.log("Send API data.");
-        socket.emit("getNewsAuto", {
-          data: data,
-        });
-      });
-      setTimeout(() => {
-        callNewsAPI(userData).then((data) => {
-          console.log("Send API data.");
-          socket.emit("getNewsAuto", {
-            data: data,
-          });
-        });
-        console.log("hehehehehhehehehehhe");
-        //Puis tout les X time on actualise ces données.
-        interval = setInterval(function () {
-          //appel api vers l'api d'actualité
-          callNewsAPI(userData).then((data) => {
-            console.log("Send API data.");
-            socket.emit("getNewsAuto", {
-              data: data,
+    //Le client demande expressement des données
+    socket.on("getNews", () => {
+        if (userData != null) {
+            callNewsAPI(userData).then((data) => {
+                console.log("Send API data.");
+                socket.emit("getNewsAuto", {
+                    data: data,
+                });
             });
-          });
-        }, getTimeCycle());
-      }, getTimeBeforeStartCycle());
-    } else {
-      console.log("Merci de spécifier des préférences utilisateur.");
-    }
-  });
+        } else {
+            console.log("Veuillez d'abord fournir vos préférences utilisateur.");
+        }
+    });
+
+    //Le client nous envoie ses filtres pour l'api d'actualité.
+    socket.on("setUserData", (sendedUserData) => {
+        userData = new UserData(
+            sendedUserData.lang,
+            sendedUserData.countries,
+            sendedUserData.topic,
+            sendedUserData.sources
+        );
+        if (userData != null) {
+            console.log("Préférences utilisateur définies.");
+            //On envoi de nous même au client les données en guise de réponse.
+            callNewsAPI(userData).then((data) => {
+                console.log("Send API data.");
+                socket.emit("getNewsAuto", {
+                    data: data,
+                });
+            });
+
+            startCycle(userData, interval, socket).then((value) => {
+                interval = value
+            })
+        } else {
+            console.log("Merci de spécifier des préférences utilisateur.");
+        }
+    });
 });
 
 server.listen(3000, () => {
-  console.log("listening on *:3000");
+    console.log("listening on *:3000");
 });
